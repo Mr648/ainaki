89236a087e9c0cc3b386907cc8124b26578c399c
rahmat waisi <rahmatwaisi@gmail.com>
2018-08-19 13:39:38 +0430

    before stash

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -7,20 +7,17 @@
                 <div class="wrapper row">
                     <div class="preview col-md-5 col-sm-12">
 
-                        <div class="preview-pic tab-content border" style="margin-bottom: 6px !important;" >
+                        <div class="preview-pic tab-content border" style="margin-bottom: 6px !important;">
+
                             <div class="tab-pane active" id="pic-1" style="height: 250px !important; ">
-                                <img
-                                        src="/images/g1.png"/></div>
-                            <div class="tab-pane" style="height: 250px !important;" id="pic-2"><img src="/images/g2.png"/>
-                            </div>
-                            <div class="tab-pane" style="height: 250px !important;" id="pic-3"><img src="/images/g3.png"/>
-                            </div>
-                            <div class="tab-pane" style="height: 250px !important;" id="pic-4"><img src="/images/g4.png"/>
+                                <img src="/images/g3.png"/>
                             </div>
 
+
                         </div>
-                        <ul class="preview-thumbnail nav nav-tabs text-center" >
-                            <li class="active border "><a class=" border" data-toggle="modal" data-target="#myModal"><img
+                        <ul class="preview-thumbnail nav nav-tabs text-center">
+                            <li class="active border "><a class=" border" data-toggle="modal"
+                                                          data-target="#myModal"><img
                                             src="/images/g1.png"/></a>
                             </li>
                             <li class=" border"><a data-toggle="modal" data-target="#myModal"><img src="/images/g2.png"
@@ -36,64 +33,10 @@
                         </ul>
 
 
-                        {{--<div class="container text-center">--}}
-
-                        {{--<h1> Click Me </h1>--}}
-                        {{--<!-- Large modal -->--}}
-                        {{--<button class="btn btn-default" data-toggle="modal" data-target=".bs-example-modal-lg">Large modal</button>--}}
-
-                        {{--<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">--}}
-                        {{--<div class="modal-dialog modal-lg">--}}
-                        {{--<div class="modal-content">--}}
-                        {{--<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">--}}
-
-
-
-                        {{--<!-- Wrapper for slides -->--}}
-                        {{--<div class="carousel-inner">--}}
-                        {{--<div class="item active">--}}
-                        {{--<img class="img-responsive" src="http://placehold.it/1200x600/555/000&text=One" alt="...">--}}
-                        {{--<div class="carousel-caption">--}}
-                        {{--One Image--}}
-                        {{--</div>--}}
-                        {{--</div>--}}
-                        {{--<div class="item">--}}
-                        {{--<img class="img-responsive" src="http://placehold.it/1200x600/fffccc/000&text=Two" alt="...">--}}
-                        {{--<div class="carousel-caption">--}}
-                        {{--Another Image--}}
-                        {{--</div>--}}
-                        {{--</div>--}}
-                        {{--<div class="item">--}}
-                        {{--<img class="img-responsive" src="http://placehold.it/1200x600/fcf00c/000&text=Three" alt="...">--}}
-                        {{--<div class="carousel-caption">--}}
-                        {{--Another Image--}}
-                        {{--</div>--}}
-                        {{--</div>--}}
-                        {{--</div>--}}
-
-                        {{--<!-- Controls -->--}}
-                        {{--<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">--}}
-                        {{--<span class="glyphicon glyphicon-chevron-left"></span>--}}
-                        {{--</a>--}}
-                        {{--<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">--}}
-                        {{--<span class="glyphicon glyphicon-chevron-right"></span>--}}
-                        {{--</a>--}}
-                        {{--</div>--}}
-                        {{--</div>--}}
-                        {{--</div>--}}
-                        {{--</div>--}}
-                        {{--</div>--}}
-
-                        <div class=" container-fluid" >
-                        {{--<h2>Centered Modal Example</h2>--}}
-                        {{--<p>Center the modal vertically and horizontally within the page, with the .modal-dialog-centered class.</p>--}}
-                        {{--<!-- Button to Open the Modal -->--}}
-                        {{--<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">--}}
-                        {{--Open modal--}}
-                        {{--</button>--}}
+                        <div class=" container-fluid">
 
                         <!-- The Modal -->
-                            <div class="modal fade " id="myModal"  >
+                            <div class="modal fade " id="myModal">
                                 <div class="modal-dialog modal-dialog-centered modal-lg">
                                     <div class="modal-content modal-test">
 
@@ -103,24 +46,27 @@
                                         </div>
 
                                         <!-- Modal body -->
-                                        <div class="modal-body "  >
-                                            <div id="demo" class="carousel slide" data-ride="carousel" >
+                                        <div class="modal-body ">
+                                            <div id="demo" class="carousel slide" data-ride="carousel">
                                                 <ul class="carousel-indicators">
                                                     <li data-target="#carousel" data-slide-to="0" class="active"></li>
                                                     <li data-target="#carousel" data-slide-to="1"></li>
                                                     <li data-target="#carousel" data-slide-to="2"></li>
                                                 </ul>
-                                                <div class="carousel-inner" >
+                                                <div class="carousel-inner">
                                                     <div class="carousel-item active">
-                                                        <img src="/images/g2.png" alt="Los Angeles"  style="height: 250px !important;" >
+                                                        <img src="/images/g2.png" alt="Los Angeles"
+                                                             style="height: 250px !important;">
 
                                                     </div>
                                                     <div class="carousel-item">
-                                                        <img src="/images/g3.png" alt="Chicago" style="height: 250px !important;" >
+                                                        <img src="/images/g3.png" alt="Chicago"
+                                                             style="height: 250px !important;">
 
                                                     </div>
                                                     <div class="carousel-item">
-                                                        <img src="/images/g4.png" alt="New York"  style="height: 250px !important;" >
+                                                        <img src="/images/g4.png" alt="New York"
+                                                             style="height: 250px !important;">
 
                                                     </div>
                                                 </div>
@@ -156,46 +102,45 @@
                     </div>
 
 
-
-                @if(isset($product->online_test_url))
-                    <div class="online-test col-md-3 col-sm-12">
-                        <br>
-                        <br>
-                        <a href="{{$product->online_test_url}}">
-                            <div class="d-flex justify-content-center w-100">
-                                <div class="rounded-circle d-flex border-ainaki feature">
-                                    <h6 class="align-self-center m-1">تست آنلاین</h6></div>
-                            </div>
-                        </a>
-                        <br>
-                        <br>
-                    </div>
-                @else
-                    <div class="col-md-3 col-sm-12">
-                    </div>
-                @endif
-                <div class="details col-md-4  col-sm-12">
-                    <h4 class="product-title">{{$product->specs->full->name}}</h4>
-                    <h6 class="product-title text-center">قیمت {{$product->price}}</h6>
-
-
-                    {{--<p class="product-description">Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia--}}
-                    {{--sem sem! Repudiandae et! Massa senectus enim minim sociosqu delectus posuere.</p>--}}
-                    <ul class="text-secondary product-description">
-                        @foreach($product->properties as $property)
-                            <li>{{$property}}</li>
-                        @endforeach
-                    </ul>
+                    @if(isset($product->online_test_url))
+                        <div class="online-test col-md-3 col-sm-12">
+                            <br>
+                            <br>
+                            <a href="{{$product->online_test_url}}">
+                                <div class="d-flex justify-content-center w-100">
+                                    <div class="rounded-circle d-flex border-ainaki feature">
+                                        <h6 class="align-self-center m-1">تست آنلاین</h6></div>
+                                </div>
+                            </a>
+                            <br>
+                            <br>
+                        </div>
+                    @else
+                        <div class="col-md-3 col-sm-12">
+                        </div>
+                    @endif
+                    <div class="details col-md-4  col-sm-12">
+                        <h4 class="product-title">{{$product->specs->full->name}}</h4>
+                        <h6 class="product-title text-center">قیمت {{$product->price}}</h6>
 
 
-                    <div class="action text-center">
-                        <a href="{{$product->add_to_basket_url}}" class="add-to-cart btn btn-default">افزودن به سبد
-                            خرید</a>
+                        {{--<p class="product-description">Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia--}}
+                        {{--sem sem! Repudiandae et! Massa senectus enim minim sociosqu delectus posuere.</p>--}}
+                        <ul class="text-secondary product-description">
+                            @foreach($product->properties as $property)
+                                <li>{{$property}}</li>
+                            @endforeach
+                        </ul>
+
+
+                        <div class="action text-center">
+                            <a href="{{$product->add_to_basket_url}}" class="add-to-cart btn btn-default">افزودن به سبد
+                                خرید</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
         <h3 class="similar-product">محصولات مشابه</h3>
 
@@ -221,7 +166,7 @@
                     <br>
                     {{--<img class="card-img-top" src="images/8.jpg" alt="Card image" style="width:400px;">--}}
                 </div>
-                <div class="card-body text-center " >
+                <div class="card-body text-center ">
                     <h5 class="card-title">عینک افتابی کومونو 879 589</h5>
                     <p class="card-text text-center">قیمت 290000 تومان</p>
                     <a href="#" class="btn btn-primary" id="see-product">مشاهده محصول</a>
@@ -248,7 +193,7 @@
                     <br>
 
                 </div>
-                <div class="card-body text-center" >
+                <div class="card-body text-center">
                     <h5 class="card-title">عینک افتابی کومونو 879 589</h5>
                     <p class="card-text">قیمت 290000 تومان</p>
                     <a href="#" class="btn btn-primary" id="see-product">مشاهده محصول</a>
@@ -275,7 +220,7 @@
                     <br>
 
                 </div>
-                <div class="card-body text-center" >
+                <div class="card-body text-center">
                     <h5 class="card-title ">عینک افتابی کومونو 879 589</h5>
                     <p class="card-text">قیمت 290000 تومان</p>
                     <a href="#" class="btn btn-primary" id="see-product">مشاهده محصول</a>
